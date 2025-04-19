b08f918c2198aad4f20cb8e68b2d07fc80e58cd6
dani0697 <122933965+dani0697@users.noreply.github.com>
2025-04-19 13:58:29 -0600

    Actulización de Vistas/Admin
    Se actualizaron las vistas de los módulos presentes en Administador y se desarrolló desde 0 la opción de Usuarios que aún no estaba desarrollada

---
+++ Proyecto Final/Controllers/UsuariosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Proyecto_Final.Models; 
+using System.Linq;
+using System.Threading.Tasks;
+
+public class UsuariosController : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public UsuariosController(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public IActionResult Index()
+    {
+        var usuarios = _userManager.Users.ToList();
+        return View(usuarios);
+    }
+}
